b24f7a1794e135630e67f5f7504537a89d1467e7
Midgetman012 <43002201+Midgetman012@users.noreply.github.com>
2018-09-05 14:38:45 +0100

    Added system pause

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,12 @@
+// Include Libraries
+#include <stdlib.h> //Standard library
+
 // Entry point for our program every c++ program needs a main() function
 int main()
 {
+	// Wait for user to press button before continuing
+	system("PAUSE");
+
 	// tell the operating system that we are done and there was no error
 	return 0;
 }
