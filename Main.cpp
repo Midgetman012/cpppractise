dcee313dcf77467223d37df98a011853dbca59a8
Midgetman012 <43002201+Midgetman012@users.noreply.github.com>
2018-09-05 14:44:29 +0100

    We are number one

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,11 @@
 // Include Libraries
 #include <stdlib.h> //Standard library
+#include <iostream> //input and output
 
 // Entry point for our program every c++ program needs a main() function
 int main()
 {
+	std::cout << "We are number one" << std::endl;
 	// Wait for user to press button before continuing
 	system("PAUSE");
 
